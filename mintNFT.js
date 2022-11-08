c8ec7f6c180933e0cd8313a8c31c27dd2c73d29a
Winter-Wub <wave_onlyone@hotmail.com>
2022-11-08 23:18:36 +0700

    update example baseuri

--- mintNFT.js
+++ mintNFT.js
@@ -1,8 +1,9 @@
 const ethers = require("ethers");
 const abi = require("./abi.json");
 require("dotenv").config();
+// MINT NFT By Your id
 
-(async () => {
+const mintNFT = async () => {
   // Prepare network provider
   const provider = new ethers.providers.JsonRpcProvider(process.env.rpcUrl);
   // Prepare signer as the Contract Owner or a Deployer
@@ -28,4 +29,31 @@ require("dotenv").config();
   );
   // this is hash.you can copy this one to explorer to check the transaction result
   console.log(trxRecipt.hash);
+};
+
+// SET URI
+const setURI = async () => {
+  // Prepare network provider
+  const provider = new ethers.providers.JsonRpcProvider(process.env.rpcUrl);
+  // Prepare signer as the Contract Owner or a Deployer
+  const ownerWallet = new ethers.Wallet(process.env.privateKey, provider);
+  // Loading Contract by ABI JSON File
+  const canItemContract = new ethers.Contract(
+    process.env.contractAddress,
+    abi,
+    provider
+  );
+  // http://localhost/:id.json
+  const canItemContractWithOwner = canItemContract.connect(ownerWallet);
+  const result = await canItemContractWithOwner.setBaseURI(
+    "https://bafybeidvbyee7rsjafvjb3cw4pz2gfuwavwbol4yj24jp6v4zx42b55kdq.ipfs.nftstorage.link/"
+  );
+  console.log(result.hash);
+};
+
+(() => {
+  // MINT NFT
+  mintNFT();
+  // SET URI
+  setURI();
 })();
